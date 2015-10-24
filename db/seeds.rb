fd6ba5d19a6806c9b796382e1ee22d321895ce4e
Maiclear <maidaclaro@gmail.com>
2015-10-23 21:28:02 -0300

    creado el seed para productos y reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+Review.destroy_all
+# Category.destroy_all
+
+
+
+# (1..20).each do |n|
+#   posts << Post.create(title: "Post nº #{n}", content: "COmentario del post numero #{n}")
+# end
+
+
+# (1..50).each do |n|
+#   comments << Comment.create( content: " Comentario numero #{n}", post: posts[rand(1..(posts.length))])
+# end
+
+products = []
+
+(1..50).each do |m|
+  products << Product.create(
+                        name: Faker::Commerce.product_name,
+                        description: Faker::Lorem.sentence,
+                        price: Faker::Commerce.price,
+                        stock: rand(1..100)
+                        )
+
+end
+
+
+reviews= []
+
+(1..20).each do |r|
+  reviews << Review.create(
+                           content: Faker::Lorem.paragraph(3)
+                           )
+end
+
+# categories=[]
+
+# (1..10).each do |r|
+#   categories << Category.create(
+#                                 name: Faker::Commerce.department
+#                                 )
