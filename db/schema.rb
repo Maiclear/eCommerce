1fb419692ba47a2a993f2cae8afcaa579a1a722a
Maiclear <maidaclaro@gmail.com>
2015-10-26 21:38:37 -0300

    agregado la migracion de Role al campo de user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026232906) do
+ActiveRecord::Schema.define(version: 20151027003757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20151026232906) do
     t.string   "name"
     t.string   "last_name"
     t.string   "user_name"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
