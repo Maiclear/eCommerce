099fb60b9912932f1fcbb4fd81b604cbb1c9f1ce
Maiclear <maidaclaro@gmail.com>
2015-10-26 21:57:24 -0300

    creado el default role para users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  before_save :default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:admin, :client, :guest]
+
+
+  def default_role
+  self.role ||= 2
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027003757) do
+ActiveRecord::Schema.define(version: 20151027004621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20151027003757) do
     t.string   "name"
     t.string   "last_name"
     t.string   "user_name"
-    t.integer  "role"
+    t.integer  "role",                   default: 2,  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
